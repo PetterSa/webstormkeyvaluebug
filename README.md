eddf2120433a3e4f8f7c65b132822ac854d7ba85
Petter Sælen <petter.saelen@gmail.com>
2020-07-31 14:44:11 +0200

    Introduce bug in WebStorm Angularlinting

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Webstormkeyvaluebug
 
+The purpose of this repository is to demonstrate and reproduce the bug reported here https://youtrack.jetbrains.com/issue/WEB-45408
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.4.
 
 ## Development server
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+interface WithContent {
+  content: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'webstormkeyvaluebug';
+  aPrettyMap: Map<string, WithContent> = new Map();
+
+  constructor() {
+    this.aPrettyMap.set('TheFirstValue', {content: 'Interesting content'});
+    this.aPrettyMap.set('TheSecondValue', {content: 'Lorem Ipsum'});
+  }
 }
